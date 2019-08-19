b2c1bc2f04de0f2ac2921d38e42896760b79728b
Sameer Shah <sameer@sameers-mbp.home>
2019-08-19 16:15:22 +0100

    Initial Commit

---
+++ src/main/java/com/restful/parentchild/ParentChildApplication.java
@@ -0,0 +1,13 @@
+package com.restful.parentchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentChildApplication.class, args);
+	}
+
+}
